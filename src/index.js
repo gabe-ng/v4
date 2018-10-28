5526f0c79186552103bbbdbfba8230ade7d6e628
Gabriel Ng <gabrielng15@gmail.com>
2018-10-28 12:40:41 -0700

    #5 Added Navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowerRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import App from "./App";
+import Navbar from "./components/Navbar";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Navbar />
   </Router>,
   document.getElementById("root")
 );
