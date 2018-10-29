ab5b92898684ec870565ec1d02899f891bed20b0
Gabriel Ng <gabrielng15@gmail.com>
2018-10-28 22:42:48 -0700

    #11 Installed react fontawesome icons and updated contact data

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,23 +1,28 @@
+import faGithub from "@fortawesome/fontawesome-free-brands/faGithub";
+import faFacebook from "@fortawesome/fontawesome-free-brands/faFacebookF";
+import faLinkedinIn from "@fortawesome/fontawesome-free-brands/faLinkedinIn";
+import faEnvelope from "@fortawesome/fontawesome-free-regular/faEnvelope";
+
 const data = [
     {
         link: "https://github.com/gabe-ng",
         label: "Github",
-        icon: "fa-github"
+        icon: faGithub
     },
     {
         link: "https://www.facebook.com/gabengatcal",
         label: "Facebook",
-        icon: "fa-facebook-f"
+        icon: faFacebook
     },
     {
         link: "https://www.linkedin.com/in/gabriel-n/",
         label: "LinkedIn",
-        icon: "fa-linkedin-in"
+        icon: faLinkedinIn
     },
     {
         link: "mailto:gabe.v.ng@gmail.com",
         label: "Email",
-        icon: "fa-envelope"
+        icon: faEnvelope
     },
 ]
 
