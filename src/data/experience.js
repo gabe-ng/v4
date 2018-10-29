65c087ce74885254f992c2d3a6e8f0200d725159
Gabriel Ng <gabrielng15@gmail.com>
2018-10-28 22:14:53 -0700

    #9 Added experience data

---
+++ src/data/experience.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        company: "General Assembly",
+        position: "Web Development Tutor",
+        description: "",
+        logo: "",
+    },
+    {
+        company: "Educents",
+        position: "Operations Analyst Intern",
+        description: "",
+        logo: ""
+    },
+    {
+        company: "People Helping People",
+        position: "Wealth Management Intern",
+        description: "",
+        logo: ""
+    },
+]
+
+export default data;
