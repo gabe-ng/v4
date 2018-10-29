460e112c8aac4795fb66a3198ecd2db8883b5779
Gabriel Ng <gabrielng15@gmail.com>
2018-10-28 22:12:13 -0700

    #8 Added contact data

---
+++ src/data/contact.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        link: "https://github.com/gabe-ng",
+        label: "Github",
+        icon: "fa-github"
+    },
+    {
+        link: "https://www.facebook.com/gabengatcal",
+        label: "Facebook",
+        icon: "fa-facebook-f"
+    },
+    {
+        link: "https://www.linkedin.com/in/gabriel-n/",
+        label: "LinkedIn",
+        icon: "fa-linkedin-in"
+    },
+    {
+        link: "mailto:gabe.v.ng@gmail.com",
+        label: "Email",
+        icon: "fa-envelope"
+    },
+]
+
+export default data;
