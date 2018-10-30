6f08bb08dcdb0dfdc14cbba0f8e100f98affd983
Gabriel Ng <gabrielng15@gmail.com>
2018-10-30 16:22:28 -0700

    #20 Fixed project data, rendering projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 
 const Projects = (props) => {
   const featured = props.data.filter(project => project.featured === true);
   const nonfeatured = props.data.filter(project => project.featured !== true);
-
-  console.log(featured)
 
   return (
     <div className="projects">
@@ -15,13 +14,30 @@ const Projects = (props) => {
       </header>
       <section>
         {featured.map(project => (
-          <article key={project.title} className="project featured">hello</article>
+          <article key={project.title} className="project featured">
+            <h2>{project.title}</h2><span>{project.date}</span>
+            <img src={`${project.image}`} alt=""/>
+            <p>{project.desc}</p>
+            <ul>
+              {project.tech.map(tech => <li className="tech">{tech}</li>)}
+            </ul>
+            <p>{project.repo}</p>
+          </article>
+        ))}
+        {nonfeatured.map(project => (
+          <article key={project.title} className="project">nonfeatured</article>
         ))}
       </section>
     </div>
   );
 };
 
+Projects.propTypes = {
+  data: PropTypes.array.isRequired,
+}
 
+Projects.defaultProps = {
+  data: [],
+}
 
 export default Projects;
