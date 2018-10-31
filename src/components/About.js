c46fb796dc4dcbfef768ea4d9f454114f4345e05
Gabriel Ng <gabrielng15@gmail.com>
2018-10-31 12:32:18 -0700

    #26 Added hobbies to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { Icon } from "react-icons-kit";
+import { triangleRight } from 'react-icons-kit/oct/triangleRight'
+
 const About = props => (
   <div className="about">
     <header>
@@ -18,20 +21,31 @@ const About = props => (
           <ul>
             <h4>Front End</h4>
             {props.data.skills.frontend.map(skill => 
-              <li key={skill} className="skill" >{skill}</li>)}
+              <li key={skill} className="skill" ><Icon icon={triangleRight} size={12} /> {skill}</li>)}
           </ul>
           <ul>
             <h4>Back End</h4>
             {props.data.skills.backend.map(skill =>
-              <li key={skill} className="skill">{skill}</li>)}
+              <li key={skill} className="skill"><Icon icon={triangleRight} size={12}/> {skill}</li>)}
           </ul>
           <ul>
             <h4>Development</h4>
             {props.data.skills.development.map(skill =>
-              <li key={skill} className="skill">{skill}</li>)}
+              <li key={skill} className="skill"><Icon icon={triangleRight} size={12}/> {skill}</li>)}
           </ul>
         </div>
       </article>
+      <article className="hobbies">
+          <h3>When I'm not coding, I'm probably</h3>
+          <ul>
+            {props.data.hobbies.map(hobby => (
+            <li key={hobby} className="hobby"><Icon icon={triangleRight} size={12} />{hobby}</li>
+            ))}
+          </ul>
+      </article>
+      <artcile className="facts">
+          {props.data.facts}
+      </artcile>
     </section>
   </div>
   );
